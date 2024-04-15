779bbdc251cbf00c8f1c57699797a6fcab2aa778
ArunRajeishbabu <arajeishbabu5302@stu.d214.org>
2024-04-15 13:14:33 -0500

    made nounview and a navigation link to verb view

---
+++ NounView.swift
@@ -0,0 +1,25 @@
+//
+//  NounView.swift
+//  GitHubPractice2
+//
+//  Created by Arun Rajeishbabu on 4/15/24.
+//
+
+import Foundation
+import SwiftUI
+struct ContentView: View {
+    var body: some View {
+        NavigationLink {
+            VerbView()
+        }
+        
+    }
+        
+        
+        
+    }
+    
+    
+    
+    
+
